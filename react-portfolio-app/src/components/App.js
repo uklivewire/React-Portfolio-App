65f0e293352cf826b11f6fca6ca524a1dbaa5aa1
uklivewire <rickybryans0@gmail.com>
2023-02-14 18:03:19 +0000

    Code added to Home.js

--- react-portfolio-app/src/components/App.js
+++ react-portfolio-app/src/components/App.js
@@ -4,20 +4,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Wrapper>
-          <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/home" element={<Home/>} />
-            <Route path="/projects" element={<Projects/>} />
-            <Route path="/contact" element={<Contact/>} />
-          </Routes>
-        </Wrapper>
-        <Footer />
-      </div>
-    </Router>
+    <div>
+      
+    </div>
   );
 }
 
---
+++ react-portfolio-app/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div className="container">
+            <h1>Ricky Bryans</h1>
+            <img src="" alt="Ricky Bryans"/>
+            <p>Front Ent Web Developer</p>
+            <p>Welcome to my portfolio</p>
+        </div>
+    );
+}
+
+export default Home;
