b1733a5ffac9fc7a840a023d72453c8356d3f604
uklivewire <rickybryans0@gmail.com>
2023-02-15 13:53:06 +0000

    updates to app.js

--- react-portfolio-app/src/components/App.js
+++ react-portfolio-app/src/components/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, } from 'react-router-dom';
 import Header from "./Header";
-import Home from "./Home"
+import Home from "./Home";
+import ProjectGallery from './ProjectGallery';
+import Project from './Project';
+import Contact from './Contact';
+import projects from "../data/projects.json"
+import "./App.css";
 
 
 function App() {
